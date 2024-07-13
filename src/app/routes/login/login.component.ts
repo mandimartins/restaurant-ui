5d31490f8eb51ed21caebc1f8e0ce87f02ba08b3
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2024-07-13 18:39:46 -0300

    feat: add form validation and error handling (#1)

--- src/app/routes/login/login.component.ts
+++ src/app/routes/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 import { Router } from '@angular/router';
+import { first, merge } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { take } from 'rxjs';
+import { ErrorBarComponent } from '../../shared/components/error-bar/error-bar.component';
 
 @Component({
   selector: 'app-login',
@@ -16,18 +17,37 @@ export class LoginComponent {
     private formBuilder: FormBuilder,
     private authService: AuthService,
     private route: Router) {
+
+      this.form = this.formBuilder.group({
+        Email: ['', [Validators.required, Validators.email]],
+        Password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(12)]]
+      })
+
+      merge(this.form.controls.Email.statusChanges,
+        this.form.controls.Email.valueChanges)
+        .pipe(takeUntilDestroyed())
+        .subscribe(() => this.updateEmailErrorMessage());
+
+      merge(this.form.controls.Password.statusChanges,
+        this.form.controls.Password.valueChages)
+        .pipe(takeUntilDestroyed())
+        .subscribe(() => this.updatePasswordErrorMessage());
   }
 
   public form: any;
+  emailErrorMessage = signal('');
+  passwordErrorMessage = signal('');
+
+  @ViewChild('errorBar') errorBar!: ErrorBarComponent;
 
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.maxLength(8)]]
-    })
+
   }
 
   public redirectUserToAdminRoute(){
+
+    //in the future it should validate if
+    //the user is admin or client, and redirect to its own panel
     this.route.navigate(['/admin'])
   }
 
@@ -37,11 +57,37 @@ export class LoginComponent {
 
     this.authService
       .auth({ 
-        UserName: user.email, 
-        Email: user.email, 
-        Password: user.password 
+        UserName: user.Email, 
+        Email: user.Email, 
+        Password: user.Password 
       })
-      .pipe(take(1))
-      .subscribe(data => this.redirectUserToAdminRoute())
+      .pipe(first())
+      .subscribe({
+        next: data => this.redirectUserToAdminRoute(),
+        error: error => this.errorBar.handleError(error)
+      })
+  }
+
+  public updateEmailErrorMessage(){
+
+    if (this.form.controls.Email.hasError('required')) {
+      this.emailErrorMessage.set('[Email] is required.');
+    } else if (this.form.controls.Email.hasError('email')) {
+      this.emailErrorMessage.set('[Email] is invalid');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+  }
+
+  public updatePasswordErrorMessage(){
+    if(this.form.controls.Password.hasError('required')){
+      this.passwordErrorMessage.set('[Password] is required')
+    }else if(this.form.controls.Password.hasError('maxlength')){
+      this.passwordErrorMessage.set('[Password] cannot have more than 12 characters')
+    }else if(this.form.controls.Password.hasError('minlength')){
+      this.passwordErrorMessage.set('[Password] must have at least 8 character')
+    }else {
+      this.emailErrorMessage.set('');
+    }
   }
 }
--- src/app/routes/login/login.module.ts
+++ src/app/routes/login/login.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCard, MatCardActions, MatCardContent } from '@angular/material/card';
+import { MatError, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
+import { ErrorBarModule } from '../../shared/components/error-bar/error-bar.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +19,13 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     LoginRoutingModule,
     ReactiveFormsModule,
+    MatCard,
+    MatCardContent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardActions,
+    MatButton,
+    ErrorBarModule
   ]
 })
 export class LoginModule { }
