29041d7facc1180278d65c58aafe12acbc686c9b
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2024-07-20 20:39:27 -0300

    refactor: replace hard coded domain and http verbs on test file (#8)
    * refactor: add environment variable
    
    * refactor: replace hard coded domain and http verbs on test file

--- src/app/routes/category/update/update.service.spec.ts
+++ src/app/routes/category/update/update.service.spec.ts
@@ -4,11 +4,15 @@ import { UpdateService } from './update.service';
 import { provideHttpClient } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { UpdateViewModel } from './update.viewmodel';
+import { HttpVerbsConstant } from '../../../shared/constants/http-verbs.constant';
+import { environment } from '../../../../environments/environment';
 
-describe('UpdateService', () => {
+fdescribe('UpdateService', () => {
 
   let updateService: UpdateService;
   let httpTestingController: HttpTestingController;
+
+  const httpVerbsConstant = HttpVerbsConstant;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -28,9 +32,9 @@ describe('UpdateService', () => {
       expect(data).toBeTruthy()
     })
 
-    const req = httpTestingController.expectOne('https://localhost:7051/api/category/add');
+    const req = httpTestingController.expectOne(`${environment.RESTAURANT_API}/category/add`);
     
-    expect(req.request.method).toBe("POST")
+    expect(req.request.method).toBe(httpVerbsConstant.POST)
 
     req.flush({Id: 1, Name:"Pen", Description: "All types of pen"})
   })
@@ -41,9 +45,9 @@ describe('UpdateService', () => {
       expect(data).toBeTruthy()
     })
     
-    const req = httpTestingController.expectOne('https://localhost:7051/api/category/get/1')
+    const req = httpTestingController.expectOne(`${environment.RESTAURANT_API}/category/get/1`)
 
-    expect(req.request.method).toBe("GET")
+    expect(req.request.method).toBe(httpVerbsConstant.GET)
 
     req.flush({Id: 1, Name:"Pen", Description: "All types of pen"})
   })
