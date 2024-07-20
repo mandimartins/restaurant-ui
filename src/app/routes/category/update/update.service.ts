7aae076120c5c4eadd0dbcaf74f098585dbde7d7
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2024-07-20 20:07:35 -0300

    refactor: add environment variable (#6)

--- src/app/routes/category/update/update.service.ts
+++ src/app/routes/category/update/update.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 
 import {UpdateViewModel} from './update.viewmodel'
 import { HttpClientService } from '../../../shared/services/http-client/http-client.service';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +13,10 @@ export class UpdateService {
   constructor(private httpClientService: HttpClientService) { }
 
   public Save(body:UpdateViewModel):Observable<unknown>{
-    return this.httpClientService.post('https://localhost:7051/api/category/add',body)
+    return this.httpClientService.post(`${environment.RESTAURANT_API}/category/add`,body)
   }
   
   public Get(id:number): Observable<unknown>{
-    return this.httpClientService.get(`https://localhost:7051/api/category/get/${id}`)
+    return this.httpClientService.get(`${environment.RESTAURANT_API}/category/get/${id}`)
   }
 }
