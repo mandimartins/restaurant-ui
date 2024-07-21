694996fe6a7c2ae68fb0827dcbec185fef995c39
Amanda Martins <mandimartins23@gmail.com>
2024-07-21 12:44:56 -0300

    refactor: separate error handling concern to another service

---
+++ src/app/shared/models/api-errormessage.ts
@@ -0,0 +1,9 @@
+export class ErrorMessage{
+    public readonly Title: string;
+    public readonly Content:string;
+  
+    constructor(private _title:string, private _content: any) {
+      this.Title = _title;
+      this.Content = _content;
+    }
+}
--- src/app/shared/services/error-handler/error-handler.service.ts
+++ src/app/shared/services/error-handler/error-handler.service.ts
@@ -1,4 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { ErrorMessage } from '../../models/api-errormessage';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,27 @@ import { Injectable } from '@angular/core';
 export class ErrorHandlerService {
 
   constructor() { }
+
+  public handleError(error: HttpErrorResponse): Observable<never> {
+    let errorMessage = '';
+    if (error.error instanceof HttpErrorResponse) 
+        errorMessage = `Server-side error: ${error.error.message}`;
+
+    console.error(errorMessage);
+
+    if(error.status === 401){
+  
+     const customError = new HttpErrorResponse({
+      error: [ new ErrorMessage('Unauthorized access','Access denied. Your session expired, try to sign in again.')],
+      headers: error.headers,
+      status: error.status,
+      statusText: error.statusText,
+      url: error.url ? error.url : ''
+     });
+
+      return throwError(() => customError)
+    }
+
+    return throwError(() => error);
+  }
 }
