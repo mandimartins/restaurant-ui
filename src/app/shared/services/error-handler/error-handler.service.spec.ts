dc473e6fea0ce1510e616207356e1bf7a052aeed
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2024-07-21 12:49:32 -0300

    refactor: separate error handling concern to another service (#9)

--- src/app/shared/services/error-handler/error-handler.service.spec.ts
+++ src/app/shared/services/error-handler/error-handler.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorHandlerService } from './error-handler.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorMessage } from '../../models/api-errormessage';
 
 describe('ErrorHandlerService', () => {
   let service: ErrorHandlerService;
@@ -13,4 +15,43 @@ describe('ErrorHandlerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should handle 401 error and return custom error', (done) => {
+    const errorMessage = 'Error message'
+    const mockErrorResponse = new HttpErrorResponse({
+      error: errorMessage,
+      status: 401,
+      statusText: 'Unauthorized'
+    });
+
+    service.handleError(mockErrorResponse).subscribe({
+      next: () => fail('Expected an error, not data'),
+      error: (error) => {
+        expect(error).toBeInstanceOf(HttpErrorResponse);
+        expect(error.status).toBe(401);
+        expect(error.error).toEqual([new ErrorMessage('Unauthorized access', 'Access denied. Your session expired, try to sign in again.')]);
+        done();
+      }
+    });
+  });
+
+  it('should handle other errors and return the original error', (done) => {
+    const errorMessage = 'Server error';
+    const mockErrorResponse = new HttpErrorResponse({
+      error: { message: errorMessage },
+      status: 500,
+      statusText: 'Server Error'
+    });
+  
+    service.handleError(mockErrorResponse).subscribe({
+      next: () => fail('Expected an error, not data'),
+      error: (error) => {
+        expect(error).toBeInstanceOf(HttpErrorResponse);
+        expect(error.status).toBe(500);
+        expect(error.error).toEqual({ message: errorMessage });
+        done();
+      }
+    });
+  });
+
 });
--- src/app/shared/services/http-client/http-client.service.ts
+++ src/app/shared/services/http-client/http-client.service.ts
@@ -1,73 +1,37 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, retry, throwError } from 'rxjs';
+import { Observable, catchError, retry } from 'rxjs';
+import { ErrorHandlerService } from '../error-handler/error-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor(private http: HttpClient) { }
-
-  private handleError(error: HttpErrorResponse): Observable<never> {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      // Client-side error
-      errorMessage = `Client-side error: ${error.error.message}`;
-    } else {
-      // Server-side error
-      errorMessage = `Server-side error: ${error.status} ${error.message}`;
-    }
-    // Log the error (you can also log to a remote server here)
-    console.error(errorMessage);
-
-    //It could be redirection as well, but  the message helps for now 
-    if(error.status === 401){
-  
-     const customError = new HttpErrorResponse({
-      error: [ new ErrorMessage('Unauthorized access','Access denied. Your session expired, try to sign in again.')],
-      headers: error.headers,
-      status: error.status,
-      statusText: error.statusText,
-      url: error.url ? error.url : ''
-     });
-
-      return throwError(() => customError)
-    }
-    // Throw an observable with a user-facing error message
-    return throwError(() => error);
-  }
+  constructor(private http: HttpClient,
+              private errorHandlerService: ErrorHandlerService
+  ) { }
 
   get(url: string, options?: { headers?: HttpHeaders, params?: HttpParams }): Observable<any> {
     return this.http.get(url, options).pipe(
       retry(2), // Retry the request up to 2 times
-      catchError(this.handleError)
+      catchError(this.errorHandlerService.handleError)
     );
   }
 
   post(url: string, body: any, options?: { headers?: HttpHeaders }): Observable<unknown> {
-    return this.http.post(url, body, options).pipe(catchError(this.handleError))
+    return this.http.post(url, body, options).pipe(catchError(this.errorHandlerService.handleError))
   }
 
   put(url: string, body: any, options?: { headers?: HttpHeaders }): Observable<unknown> {
     return this.http.put(url, body, options).pipe(
-      catchError(this.handleError)
+      catchError(this.errorHandlerService.handleError)
     );
   }
 
   delete(url: string, options?: { headers?: HttpHeaders }): Observable<unknown> {
     return this.http.delete(url, options).pipe(
-      catchError(this.handleError)
+      catchError(this.errorHandlerService.handleError)
     );
-  }
-}
-
-class ErrorMessage{
-  public readonly Title: string;
-  public readonly Content:string;
-
-  constructor(private _title:string, private _content: any) {
-    this.Title = _title;
-    this.Content = _content;
   }
 }
