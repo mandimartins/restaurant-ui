19b1dec8befe8e55268ade0fc2b04ee3c454aeeb
Amanda Martins <mandimartins23@gmail.com>
2024-07-27 19:44:13 -0300

    feat: add table of content

---
+++ src/app/routes/category/grid/grid.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClientService } from '../../../shared/services/http-client/http-client.service';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+import { GridViewModel } from './grid.viewmodel';
+import { IBaseFilter, IGridService, ITable } from '../../../shared/models/basegrid';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GridService implements IGridService<GridViewModel> {
+
+  constructor(private httpClientService: HttpClientService) { }
+
+  public getAll(payLoad:IBaseFilter): Observable<ITable<GridViewModel>>{
+    return this.httpClientService.post<ITable<GridViewModel>>(`${environment.RESTAURANT_API}/category/getall`,payLoad)
+  }
+}
